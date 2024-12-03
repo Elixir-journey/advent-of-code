b286d1e09a56c7bafc949ee96a2dba677b43dfc1
Kevin Avignon <6740474+Kavignon@users.noreply.github.com>
2024-12-03 07:23:41 -0500

    Built the skeleton for part 1.

---
+++ lib/year_2024/day_2.ex
@@ -0,0 +1,26 @@
+defmodule Year2024.Day2 do
+  @moduledoc """
+  --- Day 2: Historian Hysteria ---
+
+  The Red-Nosed Reindeer nuclear fusion/fission plant appears to contain no sign of the Chief Historian.
+  In the plant, historians are divided in groups inspecting data.
+
+  ## Part 1: How many safe reports?
+
+  The data seems to consist of many reports. Each report is a list of numbers called levels that are separated by spaces.
+  The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing.
+  7 6 4 2 1: Safe because the levels are all decreasing by 1 or 2.
+  1 2 7 8 9: Unsafe because 2 7 is an increase of 5.
+  The goal of part 1 is to count of many reports are safe from the input.
+  """
+
+  @data_path_part_1 "lib/inputs/2024/day_2/part_1.txt"
+
+  alias Infrastructure.InputFileLoader
+
+  @doc """
+  Counts of many safe reports can be observed from the input data in part_1.txt
+  """
+  def part_1 do
+  end
+end
