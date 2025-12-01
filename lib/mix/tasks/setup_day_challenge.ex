245ec366a69979976dc066bf5ed8488b64cbd4b4
Kevin Avignon <6740474+Kavignon@users.noreply.github.com>
2025-12-01 03:09:11 +0000

    Moved common helpers from setup_day_challenge.

--- lib/mix/tasks/setup_day_challenge.ex
+++ lib/mix/tasks/setup_day_challenge.ex
@@ -28,6 +28,7 @@ defmodule Mix.Tasks.SetupDayChallenge do
   use Mix.Task
 
   alias Infrastructure.Http.AocClient
+  import Mix.Tasks.AocHelpers
 
   @shortdoc "Sets up a new Advent of Code day"
 
@@ -43,41 +44,6 @@ defmodule Mix.Tasks.SetupDayChallenge do
         Mix.shell().error(message)
         Mix.shell().info("\nUsage: mix setup_day_challenge YEAR DAY")
         Mix.shell().info("Example: mix setup_day_challenge 2025 1")
-    end
-  end
-
-  defp parse_args([year_str, day_str]) do
-    with {year, ""} <- Integer.parse(year_str),
-         {day, ""} <- Integer.parse(day_str),
-         :ok <- validate_year(year),
-         :ok <- validate_day(year, day) do
-      {:ok, year, day}
-    else
-      _ -> {:error, "Invalid arguments. Year and day must be valid integers."}
-    end
-  end
-
-  defp parse_args(_), do: {:error, "Expected 2 arguments: YEAR DAY"}
-
-  @first_year 2015
-  @current_year 2025
-  defp validate_year(year) when year >= @first_year and year <= @current_year, do: :ok
-  defp validate_year(_), do: {:error, "Year must be between #{@first_year} and #{@current_year}"}
-
-  @full_advent_years 2015..2024
-  defp validate_day(year, day) do
-    cond do
-      day < 1 ->
-        {:error, "Day must be at least 1."}
-
-      day > 25 ->
-        {:error, "Day cannot exceed 25."}
-
-      day > 12 and year not in @full_advent_years ->
-        {:error, "#{year} only has challenges through December 12th."}
-
-      true ->
-        :ok
     end
   end
 
